781e12fdba290f6301d7515ddc7256472898ab65
TheCvesspy <36677487+TheCvesspy@users.noreply.github.com>
2025-12-09 17:28:00 +0100

    Update about menu page and version

--- rpg-skill-trees/rpg-skill-trees.php
+++ rpg-skill-trees/rpg-skill-trees.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: RPG Skill Trees
  * Description: Create RPG-style skill trees with tier rules, prerequisites, and build saving.
- * Version: 1.7.8
+ * Version: 1.7.17
  * Author: Quill
  * Text Domain: rpg-skill-trees
  */
